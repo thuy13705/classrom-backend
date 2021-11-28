c561e31ab61c0bfc39ee2e9a2d2f54a12fc79770
Anh Thư Phạm <thupham@Anhs-MacBook-Air.local>
2021-11-28 22:01:42 +0700

    add api remove grade

--- components/grade/index.js
+++ components/grade/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const gradeController = require('./gradeController');
 
 router.post('/add/:id',gradeController.postAddGrade);
+router.post('/delete/:id',gradeController.postDeleteGrade);
 
 module.exports = router;
