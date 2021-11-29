1e1c9dad7eda655038ab38d83e25cf90c77a6b93
ngocchi111 <18120010@student.hcmus.edu.vn>
2021-11-29 17:43:33 +0700

    Arrange assignments position Update an assignment

--- components/grade/gradeController.js
+++ components/grade/gradeController.js
@@ -28,3 +28,50 @@ exports.postAddGrade = async function (req, res) {
     res.status(500).send(error);
   }
 }
+
+exports.sort = async (req, res) =>{
+  try {
+    let message = "";
+    console.log(req.body);
+    
+    Classes.findOne({ _id: req.params.id }, function (err, classes) {
+      if (!err) {
+        classes.grades = [];
+        const grades = req.body.grades;
+        for (grade of grades)
+          classes.grades.push(grade._id);
+        classes.save();
+        message = "success";
+      }
+      else
+        message = "fail";
+    });
+
+    res.send(message);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+exports.edit = async (req, res) =>{
+  try {
+    let message = "";
+
+    console.log(req.body);
+
+    const newGrade = new Grade();
+    newGrade.name = req.body.name;
+    newGrade.point = req.body.point;
+
+    const result = await Grade.findOneAndUpdate({_id: req.body.id}, {name: req.body.name, point: req.body.point}, { upsert: true }).exec();
+    if (result) {
+      message = "success";
+    }
+    else {
+      message = "fail";
+    }
+    res.send(message);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
--- components/grade/index.js
+++ components/grade/index.js
@@ -5,4 +5,8 @@ const gradeController = require('./gradeController');
 
 router.post('/add/:id',gradeController.postAddGrade);
 
+router.post('/sort/:id',gradeController.sort);
+
+router.post('/edit/:id',gradeController.edit);
+
 module.exports = router;
