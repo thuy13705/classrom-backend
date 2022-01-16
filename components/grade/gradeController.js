1a8f3d3755bbb0f2112320eba68d2ed7256e8ee1
thuy13705 <18120587@student.hcmus.edu.vn>
2022-01-16 16:39:33 +0700

    notification

--- components/grade/gradeController.js
+++ components/grade/gradeController.js
@@ -1,5 +1,8 @@
 const Grade = require('./gradeModel');
 const Classes = require('./../classes/ClassModel')
+const User = require('../users/UserModel');
+const Notification = require('../notifications/NotificationModel');
+const mongoose = require('mongoose');
 
 exports.postAddGrade = async function (req, res) {
   try {
@@ -12,7 +15,7 @@ exports.postAddGrade = async function (req, res) {
     if (result) {
       Classes.findOne({ _id: req.params.id }, function (err, classes) {
         if (!err) {
-          classes.totalGrade=classes.totalGrade+result.point;
+          classes.totalGrade = classes.totalGrade + result.point;
           classes.grades.push(result._id);
           classes.save();
         }
@@ -30,11 +33,11 @@ exports.postAddGrade = async function (req, res) {
 }
 
 
-exports.sort = async (req, res) =>{
+exports.sort = async (req, res) => {
   try {
     let message = "";
     console.log(req.body);
-    
+
     Classes.findOne({ _id: req.params.id }, function (err, classes) {
       if (!err) {
         classes.grades = [];
@@ -54,21 +57,21 @@ exports.sort = async (req, res) =>{
   }
 }
 
-exports.edit = async (req, res) =>{
+exports.edit = async (req, res) => {
   try {
     let message = "";
     const newGrade = new Grade();
     newGrade.name = req.body.name;
     newGrade.point = req.body.point;
 
-    const _grade = await Grade.findOne({_id: req.body.id});
+    const _grade = await Grade.findOne({ _id: req.body.id });
 
-    const result = await Grade.findOneAndUpdate({_id: req.body.id}, {name: req.body.name, point: req.body.point}, { upsert: true }).exec();
+    const result = await Grade.findOneAndUpdate({ _id: req.body.id }, { name: req.body.name, point: req.body.point }, { upsert: true }).exec();
     if (result) {
       message = "success";
-      Classes.findOne({_id: req.params.id}, function(err, classes){
-        if (!err){
-          Classes.findOneAndUpdate({_id: req.params.id}, {totalGrade: classes.totalGrade - _grade.point + newGrade.point}, {upsert: true}).exec()
+      Classes.findOne({ _id: req.params.id }, function (err, classes) {
+        if (!err) {
+          Classes.findOneAndUpdate({ _id: req.params.id }, { totalGrade: classes.totalGrade - _grade.point + newGrade.point }, { upsert: true }).exec()
         }
       })
     }
@@ -81,50 +84,51 @@ exports.edit = async (req, res) =>{
   }
 }
 exports.postDeleteGrade = async function (req, res) {
-  try{
-  let message = "";
-    Classes.findOne({_id: req.params.id}, function(err, classes){
-      if(!err){
-        Grade.findOneAndDelete({_id: req.body.id}, function(error, grade){
-          if(!error){
-            Classes.findOneAndUpdate({_id: req.params.id}, {totalGrade: classes.totalGrade - grade.point, 
-                                        $pull: {grades: req.body.id}}, {upsert: true}).exec()
+  try {
+    let message = "";
+    Classes.findOne({ _id: req.params.id }, function (err, classes) {
+      if (!err) {
+        Grade.findOneAndDelete({ _id: req.body.id }, function (error, grade) {
+          if (!error) {
+            Classes.findOneAndUpdate({ _id: req.params.id }, {
+              totalGrade: classes.totalGrade - grade.point,
+              $pull: { grades: req.body.id }
+            }, { upsert: true }).exec()
           }
         })
         message = "succcess";
       }
-      else{
+      else {
         message = "fail";
       }
     })
     res.send(message);
-  }catch(error){
+  } catch (error) {
     res.status(500).send(error);
   }
 }
 
-exports.pointAllGrade = async (req, res) =>{
+exports.pointAllGrade = async (req, res) => {
   try {
     let message = "";
 
-    const result = await Grade.findOne({_id: req.params.id});
+    const result = await Grade.findOne({ _id: req.params.id });
 
     const datas = req.body.datas;
 
-    if (result){
-      for (data of datas){
+    if (result) {
+      for (data of datas) {
         let tmp = true;
         for (point of result.pointStudent)
-          if(data.studentID === point.studentID)
-            {
-              tmp=false;
-              point.point = data.point;
-              break;
-            }
-        if (tmp) 
-            result.pointStudent.push(data);
+          if (data.studentID === point.studentID) {
+            tmp = false;
+            point.point = data.point;
+            break;
+          }
+        if (tmp)
+          result.pointStudent.push(data);
       }
-      Grade.findOneAndUpdate({_id: req.params.id}, {pointStudent: result.pointStudent}, {upsert: true}).exec();
+      Grade.findOneAndUpdate({ _id: req.params.id }, { pointStudent: result.pointStudent }, { upsert: true }).exec();
     }
     else {
       message = "fail";
@@ -135,22 +139,21 @@ exports.pointAllGrade = async (req, res) =>{
   }
 }
 
-exports.sendPoint = async (req, res) =>{
+exports.sendPoint = async (req, res) => {
   try {
     let message = "";
 
-    const result = await Grade.findOne({_id: req.params.id});
+    const result = await Grade.findOne({ _id: req.params.id });
 
     const body = req.body;
-    if (result){
-        for (point of result.pointStudent)
-          if(body.studentID === point.studentID)
-            {
-              tmp=false;
-              point.point = body.point;
-              break;
-            }
-      Grade.findOneAndUpdate({_id: req.params.id}, {pointStudent: result.pointStudent}, {upsert: true}).exec();
+    if (result) {
+      for (point of result.pointStudent)
+        if (body.studentID === point.studentID) {
+          tmp = false;
+          point.point = body.point;
+          break;
+        }
+      Grade.findOneAndUpdate({ _id: req.params.id }, { pointStudent: result.pointStudent }, { upsert: true }).exec();
     }
     else {
       message = "fail";
@@ -161,14 +164,28 @@ exports.sendPoint = async (req, res) =>{
   }
 }
 
-exports.markFinal = async (req, res) =>{
+exports.markFinal = async (req, res) => {
   try {
     let message = "";
 
-    const result = await Grade.findOne({_id: req.params.id});
+    const result = await Grade.findOne({ _id: req.params.id });
 
-    if (result){
-      Grade.findOneAndUpdate({_id: req.params.id}, {isFinal: true}, {upsert: true}).exec();
+    if (result) {
+      await Grade.findOneAndUpdate({ _id: req.params.id }, { isFinal: true }, { upsert: true }).exec();
+      for (student of result.pointStudent) {
+        const userResult = await User.findOne({ studentID: student.studentID }).exec();
+        const idStudent = new mongoose.Types.ObjectId(userResult._id);
+        const idGrade = new mongoose.Types.ObjectId(result._id);
+        const isClass = await Classes.find({ students: idStudent, grades: idGrade }).exec();
+        if (isClass.length>0) {
+          var newNoti = new Notification();
+          newNoti.grade=result._id;
+          newNoti.description ="finished";
+          newNoti.userRecieve=req.user.id;
+          newNoti.user=userResult._id;
+          await newNoti.save();
+        }
+      }
     }
     else {
       message = "fail";
